fe786a21f30f9174250c471f8fb85f9fa42b8033
barsharani28 <maharanabarsharani28@gmail.com>
2024-10-14 16:10:15 +0530

    kjh

--- ComcastCRMGUIFramework/src/main/java/com/comcast/crm/objectRepositryutility/CreateProductPage.java
+++ ComcastCRMGUIFramework/src/main/java/com/comcast/crm/objectRepositryutility/CreateProductPage.java
@@ -8,5 +8,8 @@ import org.openqa.selenium.support.PageFactory;
 import com.comcast.crm.generic.webdriverutility.WebDriverUtility;
 
 public class CreateNewOrganizationPage {
-//code
+
+	public static void main(String[] args) {
+		
+	}
 }
