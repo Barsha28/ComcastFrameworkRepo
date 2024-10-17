cf4a160bfa356b82a68ddb3a6740bcbe3f20cb3b
barsharani28 <maharanabarsharani28@gmail.com>
2024-10-17 14:51:10 +0530

    Merge branch 'master' of https://github.com/Barsha28/ComcastFrameworkRepo.git

--- ComcastCRMGUIFramework/src/main/java/com/comcast/crm/objectRepositryutility/CreateProductPage.java
+++ ComcastCRMGUIFramework/src/main/java/com/comcast/crm/objectRepositryutility/CreateProductPage.java
@@ -1,13 +1,6 @@
 package com.comcast.crm.objectRepositryutility;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
-
-import com.comcast.crm.generic.webdriverutility.WebDriverUtility;
-
-public class CreateNewOrganizationPage {
+public class CreateProductPage {
 
 	public static void main(String[] args) {
 		//code
